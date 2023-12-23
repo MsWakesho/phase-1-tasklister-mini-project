ceca651ccb96c619c2dc5817163ebbcfad043afc
MsWakesho <wakeshosonja@gmail.com>
2023-12-23 15:47:41 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskInput = document.getElementById('taskInput');
+  let submitTaskButton = document.getElementById('submitTask');
+  let taskList = document.getElementById('taskList');
+
+  submitTaskButton.addEventListener('click', function () {
+    
+    const task = taskInput.value.trim();
+
+    
+    if (task !== '') {
+     
+      const listItem = document.createElement('li');
+      listItem.textContent = task;
+
+      
+      taskList.appendChild(listItem);
+
+    
+      taskInput.value = '';
+    }
+  });
 });
+
